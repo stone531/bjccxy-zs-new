13094fc99b6ef4f8d821dd701cc1eff8e408b920
stone <lirulun@xiaomi.com>
2025-07-07 22:23:41 +0800

    zsquery.vue show ok

--- web/src/view/external/zsQuery.vue
+++ web/src/view/external/zsQuery.vue
@@ -14,76 +14,77 @@
         />
       </div>
 
-       <!-- md:w-5/5 w-10/12 h-full flex items-center justify-evenly -->
-      <div class="md:w-full w-10/12 h-full flex items-center justify-evenly relative">
-        <div class="h-0 w-32 bg-white dark:bg-slate-900 absolute -ml-8" />
+       <div class="md:w-full w-10/12 h-full flex items-center justify-evenly relative">
+    <div class="h-0 w-32 bg-white dark:bg-slate-900 absolute -ml-8" />
 
-        <div
-          class="fixed right-[10rem] top-1/2 -translate-y-1/2 z-[999] w-[30rem] rounded-lg flex flex-col justify-between bg-white dark:bg-slate-800 p-6 shadow-lg"
+    <div
+      class="fixed right-[10rem] top-1/2 -translate-y-1/2 z-[999] w-[30rem] rounded-lg flex flex-col justify-between bg-white dark:bg-slate-800 p-6 shadow-lg"
+    >
+      <!-- Tab 标签页 -->
+      <el-tabs v-model="activeTab" type="card" class="mb-4">
+        <el-tab-pane label="证书查询" name="certificate" />
+        <el-tab-pane label="学籍查询" name="school" />
+        <el-tab-pane label="培训证书查询" name="train" />
+      </el-tabs>
+
+      <el-form
+        ref="queryForm"
+        :model="formData"
+        :rules="rules"
+        label-width="100px"
+      >
+        <!-- 姓名 -->
+        <el-form-item label="姓名：" prop="name" class="mb-6">
+          <el-input
+            v-model="formData.name"
+            size="large"
+            placeholder="请输入姓名"
+          />
+        </el-form-item>
+
+        <!-- 根据 tab 控制显示字段 -->
+        <!-- 证书号：只在 certificate 页显示 -->
+        <el-form-item
+          v-if="activeTab === 'certificate'"
+          label="证书号："
+          prop="number"
+          class="mb-6"
         >
-          <div>
-            
-            <el-form
-              ref="loginForm"
-              :model="loginFormData"
-              :rules="rules"
-              :validate-on-rule-change="false"
-              @keyup.enter="submitForm"
-              label-width="80px"
-            >
-              <el-form-item label="姓 名:"  prop="name" class="mb-6">
-                <el-input
-                  v-model="loginFormData.username"
-                  size="large"
-                  placeholder="请输入姓名"
-                />
-              </el-form-item>
-              <el-form-item  label="证书号:" prop="password" class="mb-6">
-                <el-input
-                  v-model="loginFormData.password"
-                  size="large"
-                  type="password"
-                  placeholder="请输入证书号"
-                />
-              </el-form-item>
-              <el-form-item
-                label="验证码:"
-                v-if="loginFormData.openCaptcha"
-                prop="captcha"
-                class="mb-6"
-              >
-                <div class="flex w-full justify-between">
-                  <el-input
-                    v-model="loginFormData.captcha"
-                    placeholder="请输入验证码"
-                    size="large"
-                    class="flex-1 mr-5"
-                  />
-                  <div class="w-1/3 h-11 bg-[#c3d4f2] rounded">
-                    <img
-                      v-if="picPath"
-                      class="w-full h-full"
-                      :src="picPath"
-                      alt="请输入验证码"
-                      @click="loginVerify()"
-                    />
-                  </div>
-                </div>
-              </el-form-item>
-              <el-form-item class="mb-6">
-                <el-button
-                  class="shadow shadow-active h-11 w-full"
-                  type="primary"
-                  size="large"
-                  @click="submitForm"
-                  >证书查询</el-button
-                >
-              </el-form-item>
-              
-            </el-form>
-          </div>
-        </div>
-      </div>
+          <el-input
+            v-model="formData.number"
+            size="large"
+            placeholder="请输入证书号"
+          />
+        </el-form-item>
+
+        <!-- 身份证号：在 school / train 页显示 -->
+        <el-form-item
+          v-if="activeTab !== 'certificate'"
+          label="身份证号："
+          prop="idcard"
+          class="mb-6"
+        >
+          <el-input
+            v-model="formData.idcard"
+            size="large"
+            placeholder="请输入身份证号"
+          />
+        </el-form-item>
+
+        <!-- 查询按钮 -->
+        <el-form-item class="mb-6">
+          <el-button
+            class="shadow shadow-active h-11 w-full"
+            type="primary"
+            size="large"
+            @click="submitQuery"
+          >
+            {{ tabButtonText[activeTab] }}
+          </el-button>
+        </el-form-item>
+      </el-form>
+    </div>
+  </div>
       
     </div>
 
@@ -96,6 +97,7 @@
 
 
 <script setup>
+//http://localhost:8080/#/external-query
   import { captcha } from '@/api/user'
   import { checkDB } from '@/api/initdb'
   import BottomInfo from '@/components/bottomInfo/bottomInfo.vue'
@@ -105,7 +107,7 @@
   import { useUserStore } from '@/pinia/modules/user'
 
   defineOptions({
-    name: 'Login'
+    name: 'zsQuery'
   })
 
   const router = useRouter()
@@ -123,6 +125,54 @@
     } else {
       callback()
     }
+  }
+
+  const activeTab = ref('certificate') // 当前页：certificate / school / train
+  // 表单字段
+  const formData = reactive({
+    name: '',
+    number: '',  // 用于证书号
+    idcard: '',  // 用于身份证号
+  })
+
+  const tabButtonText = {
+    certificate: '证书查询',
+    school: '学籍查询',
+    train: '培训证书查询',
+  }
+
+  // 表单校验规则
+  const rules = {
+    name: [{ required: true, message: '请输入姓名', trigger: 'blur' }],
+    number: [
+      {
+        required: () => activeTab.value === 'certificate',
+        message: '请输入证书号',
+        trigger: 'blur',
+      },
+    ],
+    idcard: [
+      {
+        required: () => activeTab.value !== 'certificate',
+        message: '请输入身份证号',
+        trigger: 'blur',
+      },
+    ],
+  }
+
+  // 提交查询
+const submitQuery = () => {
+  const typeMap = {
+    certificate: '证书查询',
+    school: '学籍查询',
+    train: '培训证书查询',
+  }
+
+  const payload = {
+    type: activeTab.value, // certificate / school / train
+    name: formData.name,
+    number: formData.number,
+    idcard: formData.idcard,
   }
 
   // 获取验证码
@@ -150,21 +200,12 @@
     captchaId: '',
     openCaptcha: false
   })
-  const rules = reactive({
-    username: [{ validator: checkUsername, trigger: 'blur' }],
-    password: [{ validator: checkPassword, trigger: 'blur' }],
-    captcha: [
-      {
-        message: '验证码格式不正确',
-        trigger: 'blur'
-      }
-    ]
-  })
 
   const userStore = useUserStore()
   const login = async () => {
     return await userStore.LoginIn(loginFormData)
   }
+
   const submitForm = () => {
     loginForm.value.validate(async (v) => {
       if (!v) {
@@ -191,5 +232,5 @@
       return true
     })
   }
-
+}
 </script>
