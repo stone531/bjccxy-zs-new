4ac640827720a2c71d0f2aa1e86fbbdc76f6e708
stone <lirulun@xiaomi.com>
2025-07-07 21:54:00 +0800

    fix zsquery.vue one query

--- web/src/view/external/zsQuery.vue
+++ web/src/view/external/zsQuery.vue
@@ -1,38 +1,195 @@
 <template>
-  <div class="external-query">
-    <el-card class="box-card">
-      <h2>外部查询入口</h2>
-      <el-form :model="form">
-        <el-form-item label="用户名">
-          <el-input v-model="form.username" />
-        </el-form-item>
-        <el-form-item label="密码">
-          <el-input v-model="form.password" type="password" />
-        </el-form-item>
-        <el-form-item>
-          <el-button type="primary" @click="handleQuery">查询</el-button>
-        </el-form-item>
-      </el-form>
-      <div v-if="result">
-        <h3>查询结果：</h3>
-        <pre>{{ result }}</pre>
+  <div id="userLayout" class="w-full h-full relative">
+    <div
+      class="rounded-lg flex items-center justify-evenly w-full h-full md:w-screen md:h-screen md:bg-[#194bfb] bg-white"
+    >
+       <!-- class="h-full" -->
+      <div class="min-h-screen bg-cover bg-center bg-no-repeat">
+        <img
+          
+          class="absolute inset-0 w-full h-full object-cover object-top mt-0"
+          
+          src="@/assets/bjccxy/banner.jpg"
+          alt="banner"
+        />
       </div>
-    </el-card>
+
+       <!-- md:w-5/5 w-10/12 h-full flex items-center justify-evenly -->
+      <div class="md:w-full w-10/12 h-full flex items-center justify-evenly relative">
+        <div class="h-0 w-32 bg-white dark:bg-slate-900 absolute -ml-8" />
+
+        <div
+          class="fixed right-[10rem] top-1/2 -translate-y-1/2 z-[999] w-[30rem] rounded-lg flex flex-col justify-between bg-white dark:bg-slate-800 p-6 shadow-lg"
+        >
+          <div>
+            
+            <el-form
+              ref="loginForm"
+              :model="loginFormData"
+              :rules="rules"
+              :validate-on-rule-change="false"
+              @keyup.enter="submitForm"
+              label-width="80px"
+            >
+              <el-form-item label="姓 名:"  prop="name" class="mb-6">
+                <el-input
+                  v-model="loginFormData.username"
+                  size="large"
+                  placeholder="请输入姓名"
+                />
+              </el-form-item>
+              <el-form-item  label="证书号:" prop="password" class="mb-6">
+                <el-input
+                  v-model="loginFormData.password"
+                  size="large"
+                  type="password"
+                  placeholder="请输入证书号"
+                />
+              </el-form-item>
+              <el-form-item
+                label="验证码:"
+                v-if="loginFormData.openCaptcha"
+                prop="captcha"
+                class="mb-6"
+              >
+                <div class="flex w-full justify-between">
+                  <el-input
+                    v-model="loginFormData.captcha"
+                    placeholder="请输入验证码"
+                    size="large"
+                    class="flex-1 mr-5"
+                  />
+                  <div class="w-1/3 h-11 bg-[#c3d4f2] rounded">
+                    <img
+                      v-if="picPath"
+                      class="w-full h-full"
+                      :src="picPath"
+                      alt="请输入验证码"
+                      @click="loginVerify()"
+                    />
+                  </div>
+                </div>
+              </el-form-item>
+              <el-form-item class="mb-6">
+                <el-button
+                  class="shadow shadow-active h-11 w-full"
+                  type="primary"
+                  size="large"
+                  @click="submitForm"
+                  >证书查询</el-button
+                >
+              </el-form-item>
+              
+            </el-form>
+          </div>
+        </div>
+      </div>
+      
+    </div>
+
+    <BottomInfo class="left-0 right-0 absolute bottom-3 mx-auto w-full z-20">
+      
+    </BottomInfo>
   </div>
 </template>
 
+
+
 <script setup>
-import { ref } from 'vue'
-import { externalQuery } from '@/api/externalQuery'
+  import { captcha } from '@/api/user'
+  import { checkDB } from '@/api/initdb'
+  import BottomInfo from '@/components/bottomInfo/bottomInfo.vue'
+  import { reactive, ref } from 'vue'
+  import { ElMessage } from 'element-plus'
+  import { useRouter } from 'vue-router'
+  import { useUserStore } from '@/pinia/modules/user'
 
-const form = ref({
-  username: '',
-  password: '',
-})
-const result = ref(null)
+  defineOptions({
+    name: 'Login'
+  })
 
-const handleQuery = async () => {
-  const res = await externalQuery(form.value)
-  result.value = res.data
-}
+  const router = useRouter()
+  // 验证函数
+  const checkUsername = (rule, value, callback) => {
+    if (value.length < 5) {
+      return callback(new Error('请输入正确的用户名'))
+    } else {
+      callback()
+    }
+  }
+  const checkPassword = (rule, value, callback) => {
+    if (value.length < 6) {
+      return callback(new Error('请输入正确的密码'))
+    } else {
+      callback()
+    }
+  }
+
+  // 获取验证码
+  const loginVerify = async () => {
+    const ele = await captcha()
+    rules.captcha.push({
+      max: ele.data.captchaLength,
+      min: ele.data.captchaLength,
+      message: `请输入${ele.data.captchaLength}位验证码`,
+      trigger: 'blur'
+    })
+    picPath.value = ele.data.picPath
+    loginFormData.captchaId = ele.data.captchaId
+    loginFormData.openCaptcha = ele.data.openCaptcha
+  }
+  loginVerify()
+
+  // 登录相关操作
+  const loginForm = ref(null)
+  const picPath = ref('')
+  const loginFormData = reactive({
+    username: 'admin',
+    password: '',
+    captcha: '',
+    captchaId: '',
+    openCaptcha: false
+  })
+  const rules = reactive({
+    username: [{ validator: checkUsername, trigger: 'blur' }],
+    password: [{ validator: checkPassword, trigger: 'blur' }],
+    captcha: [
+      {
+        message: '验证码格式不正确',
+        trigger: 'blur'
+      }
+    ]
+  })
+
+  const userStore = useUserStore()
+  const login = async () => {
+    return await userStore.LoginIn(loginFormData)
+  }
+  const submitForm = () => {
+    loginForm.value.validate(async (v) => {
+      if (!v) {
+        // 未通过前端静态验证
+        ElMessage({
+          type: 'error',
+          message: '请正确填写登录信息',
+          showClose: true
+        })
+        await loginVerify()
+        return false
+      }
+
+      // 通过验证，请求登陆
+      const flag = await login()
+
+      // 登陆失败，刷新验证码
+      if (!flag) {
+        await loginVerify()
+        return false
+      }
+
+      // 登陆成功
+      return true
+    })
+  }
+
 </script>
