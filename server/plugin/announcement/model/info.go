ddff771c741a9122332d236a7de1c276e24e4250
stone <lirulun@126.com>
2025-07-08 13:13:19 +0800

    fix grom run bug

--- server/plugin/announcement/model/info.go
+++ server/plugin/announcement/model/info.go
@@ -2,7 +2,7 @@ package model
 
 import (
 	"github.com/flipped-aurora/gin-vue-admin/server/global"
-	"gorm.io/datatypes"
+	//"gorm.io/datatypes"
 )
 
 // Info 公告 结构体
@@ -11,7 +11,8 @@ type Info struct {
 	Title       string         `json:"title" form:"title" gorm:"column:title;comment:公告标题;"`                                              //标题
 	Content     string         `json:"content" form:"content" gorm:"column:content;comment:公告内容;type:text;"`                              //内容
 	UserID      *int           `json:"userID" form:"userID" gorm:"column:user_id;comment:发布者;"`                                           //作者
-	Attachments datatypes.JSON `json:"attachments" form:"attachments" gorm:"column:attachments;comment:相关附件;" swaggertype:"array,object"` //附件
+	//Attachments datatypes.JSON `json:"attachments" form:"attachments" gorm:"column:attachments;comment:相关附件;" swaggertype:"array,object"` //附件
+	Attachments string          `json:"attachments" form:"attachments" gorm:"column:attachments;comment:相关附件;" swaggertype:"array,object"` //附件
 }
 
 // TableName 公告 Info自定义表名 gva_announcements_info
